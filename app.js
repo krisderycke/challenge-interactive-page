5d114bc721db7ac05e804545c374b56b3b4a2a07
Kris de Rycke <derycke.kris@gmail.com>
2019-08-01 16:27:03 +0200

    finish js function to hide and show tabs

--- app.js
+++ app.js
@@ -16,18 +16,29 @@
 function click1() {
   let content1 = document.getElementById("tabcontent1");
   console.log(content1);
-  content1.style.backgroundColor = "green";
-  content1.style.display = "none";
+  if (content1.style.display === "none") {
+    content1.style.display = "block";
+  } else {
+    content1.style.display = "none";
+  }
 }
 
 function click2() {
   let content2 = document.getElementById("tabcontent2");
   console.log(content2);
-  content2.style.backgroundColor = "green";
+  if (content2.style.display === "none") {
+    content2.style.display = "block";
+  } else {
+    content2.style.display = "none";
+  }
 }
 
 function click3() {
   let content3 = document.getElementById("tabcontent3");
   console.log(content3);
-  content3.style.backgroundColor = "green";
+  if (content3.style.display === "none") {
+    content3.style.display = "block";
+  } else {
+    content3.style.display = "none";
+  }
 }
