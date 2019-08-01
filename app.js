a0b4b53333aa02aa2f21d9ba09fa9b23cf4ebbc4
Kris de Rycke <derycke.kris@gmail.com>
2019-08-01 14:02:01 +0200

    rewrite JS function

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-function openDay(evt, Day) {
-  // Declare all variables
-  let tabcontent, tablinks;
+let tabs = document.getElementsByClassName("tablinks");
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", showTab);
+  console.log(tabs[i]);
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  tablinks = document.getElementsByClassName("tablinks");
-  for (let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementsByClassName(Day).style.display = "block";
-    evt.currentTarget.className += " active";
-    console.log(Day);
+  function showTab(e) {
+    let contents = document.getElementsByClassName("tabcontent");
+    for (let x = 0; x < contents.length; x++) {
+      console.log(contents[x]);
+      // e.contents[x].style.backgroundColor = "green";
+      // e.contents[i].style.display = "none";
+    }
   }
 }
